e7cc84e34a34379aaa5873ff941bb237a34e9fc3
torrua <torrua@gmail.com>
2020-07-02 02:47:00 +0700

    ADD todos

--- converters/convert.py
+++ converters/convert.py
@@ -8,6 +8,8 @@ from converters.txt_to_pg.txt_to_pg_execute import convert_txt_to_pg
 from converters.txt_to_ac.txt_to_ac_execute import convert_txt_to_ac
 
 # TODO sync field types with mdb
+# TODO Activate source selection
+# TODO Unify logging and resulting
 
 SELECTOR = 4
 
